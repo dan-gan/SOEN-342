9f5147d559ab29453a79bc50db733056939fc788
Hildthelsta <d_cazac@live.concordia.ca>
2026-03-19 07:30:46 -0400

    Added Basic Classes and Enum
    - Added ActivityEntry
    - Added PriorityLevel
    - Added Tag
    - Added Task
    - Added TaskStatus
    - Added Subtask

--- src/main/java/PTMS.java
+++ src/main/java/PTMS.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
 public class PTMS {
     public static void main(String[] args){
 
-        System.out.println("Hello, World!");
+
     }
 }
---
+++ src/main/java/TaskStatus.java
@@ -0,0 +1,5 @@
+public enum TaskStatus {
+    OPEN,
+    COMPLETED,
+    CANCELLED
+}
