934bb574cc6e32b8c6bf152f91abc7c1a2bc14c3
Daniel <danielganchev649@gmail.com>
2026-03-23 15:28:44 -0400

    Added setters/getters/toString to Project

--- src/main/java/Project.java
+++ src/main/java/Project.java
@@ -2,18 +2,39 @@ import java.util.List;
 
 public class Project {
     private int id;
-    private String title;
+    private String title;       //mandatory
     private String description; //optional
-    private List<Task> tasks;
+    private List<Task> tasks;   //0..*
 
     public Project(){
 
     }
 
     public Project(String title){
-
+        this();
+        this.title = title;
     }
 
+    //GETTERS / SETTERS
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setDescription(String desc){
+        this.description = desc;
+    }
+
+    public List<Task> getTasks(){
+        return this.tasks;
+    }
 
     public void addTask(Task t){
         this.tasks.add(t);
@@ -21,5 +42,14 @@ public class Project {
 
     public void removeTask(Task t){
         this.tasks.remove(t);
+    }
+
+    @Override
+    public String toString(){
+        String result = "Project Title: " + this.title + "\nDescription: " + this.description + "\n";
+        for(Task t : tasks){
+            result += t + "\n";
+        }
+        return result;
     }
 }
