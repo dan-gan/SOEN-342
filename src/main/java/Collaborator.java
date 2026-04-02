4ce41d28fa59526bd50a44772cbed2c9327bec4a
Hildthelsta <131473217+Hildthelsta@users.noreply.github.com>
2026-04-02 00:59:18 -0400

    Implemented #16
    - Added ProjectCatalogue
    - Added CollaboratorCatalogue
    - Added the mehtod in TaskManager to see Overloaded Collaborators
    - Added Method to getOpenTaskCount, limit and the variable for the task limits for each category

--- src/main/java/Collaborator.java
+++ src/main/java/Collaborator.java
@@ -5,6 +5,9 @@ public class Collaborator {
     private String name;
     private CollaboratorCategory category;
     private List<Task> tasks = new ArrayList<>();
+    public static int limitForJuniors = 10;
+    public static int limitForIntermediate = 5;
+    public static int limitForSeniors = 2;
 
     public Collaborator(String name, CollaboratorCategory category) {
         this.name = name;
@@ -18,6 +21,47 @@ public class Collaborator {
     
     public List<Task> getTasks() { return tasks; }
     public void addTask(Task task) { tasks.add(task); }
+
+    public int getOpenTaskCount(){
+        int count = 0;
+        for(Task task: tasks){
+            if(task.getStatus() == Status.OPEN){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean isOverloaded(){
+        int countOpenAssignedTasks = getOpenTaskCount();
+        if(category == CollaboratorCategory.JUNIOR && countOpenAssignedTasks > limitForJuniors){
+            return true;
+        }
+        else if(category == CollaboratorCategory.INTERMEDIATE && countOpenAssignedTasks > limitForIntermediate){
+            return true;
+        }
+        else if(category == CollaboratorCategory.SENIOR && countOpenAssignedTasks > limitForSeniors){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public int getLimit(){
+        if(category == CollaboratorCategory.JUNIOR){
+            return limitForJuniors;
+        }
+        else if(category == CollaboratorCategory.INTERMEDIATE){
+            return limitForIntermediate;
+        }
+        else if(category == CollaboratorCategory.SENIOR){
+            return limitForSeniors;
+        }
+        else{
+            return 0;
+        }
+    }
 }
 
 
